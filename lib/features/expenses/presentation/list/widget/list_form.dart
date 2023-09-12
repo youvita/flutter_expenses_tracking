a7e46ab04583398be31491204f6dbd006c4ba140
youvita <chanyouvita@gmail.com>
2023-09-12 16:28:23 +0700

    - updated expense list

--- lib/features/expenses/presentation/list/widget/list_form.dart
+++ lib/features/expenses/presentation/list/widget/list_form.dart
@@ -18,26 +18,38 @@ class ListFormWidget extends StatefulWidget {
 class _ListFormState extends State<ListFormWidget> {
   var lastHeader = "";
   var lastChildHeader = "";
+  var totalAmount = 0.0;
 
   @override
   Widget build(BuildContext context) {
     List<Expenses> listItem = List.empty();
     List<String> listHeader = List.empty(growable: true);
-    List<String> listChildHeader = List.empty(growable: true);
+    List<double> listTotal = List.empty(growable: true);
 
     setState(() {
       listItem = context.select((ListExpenseBloc bloc) => bloc.state.listExpenses ?? List.empty());
 
+      /// loop to filter header each year
       for (var i = 0; i < listItem.length; i++) {
         DateTime dateTime = Utils.dateTimeFormat("${listItem.elementAt(i).createDate}");
         String header = Utils.dateFormatYear(dateTime);
-        String childHeader = Utils.dateFormatYearMonth(dateTime);
         if (lastHeader != header) {
           listHeader.add(header);
           lastHeader = header;
         }
       }
     });
+
+    /// loop to sum total amount each year
+    for (var i = 0; i < listHeader.length; i++) {
+      for(var j = i; j < listItem.length; j++) {
+        if (listHeader.elementAt(i) == Utils.dateFormatYear(Utils.dateTimeFormat("${listItem.elementAt(j).createDate}"))) {
+          totalAmount = totalAmount + double.parse(listItem.elementAt(j).amount ?? "");
+        }
+      }
+      listTotal.add(totalAmount);
+      totalAmount = 0.0;
+    }
 
     return ListView.builder(
         itemCount: listHeader.length,
@@ -49,8 +61,10 @@ class _ListFormState extends State<ListFormWidget> {
                     padding: const EdgeInsets.only(left: 20, top: 7, right: 20, bottom: 7),
                     child: Row(
                       mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text(listHeader.elementAt(index), style: MyTextStyles.textStyleMedium17)
+                        Text(listHeader.elementAt(index), style: MyTextStyles.textStyleMedium17),
+                        Text(listTotal.elementAt(index).toString(), style: MyTextStyles.textStyleMedium17Red),
                       ],
                     )
                 ),
@@ -59,59 +73,30 @@ class _ListFormState extends State<ListFormWidget> {
                   physics: const ScrollPhysics(),
                   child: Column(
                       children: [
-                            ListView.builder(
-                                physics: const NeverScrollableScrollPhysics(),
-                                shrinkWrap: true,
-                                itemCount: listItem.length,
-                                itemBuilder: (context, indexChild) {
-                                  Expenses? childItem = listItem.elementAt(indexChild);
-                                  return listHeader.elementAt(index) == Utils.dateFormatYear(Utils.dateTimeFormat("${childItem.createDate}")) ?
-                                  Column(
-                                    children: [
-                                      ListItem(item: childItem),
-                                      childItem == listItem.last ? const SizedBox()
-                                          : Container(
-                                          padding: const EdgeInsets.only(left: 70),
-                                          child: const Divider(
-                                              height: 0,
-                                              thickness: 1
-                                          ))
-                                    ],
-                                  ) : const SizedBox();
-                                }
-                            )
-                      ]))
-
-              // ExpansionTile(
-              //   title: Text("Header"),
-              //   children: [
-              //     isTile ?
-              //     Column(
-              //       children: [
-              //         ListItem(item: item),
-              //         item == listItem?.last ? const SizedBox()
-              //             : Container(
-              //             padding: const EdgeInsets.only(left: 70),
-              //             child: const Divider(
-              //                 height: 0,
-              //                 thickness: 1
-              //             ))
-              //       ],
-              //     ):
-              //     Column(
-              //       children: [
-              //         ListItem(item: item),
-              //         item == listItem?.last ? const SizedBox()
-              //             : Container(
-              //             padding: const EdgeInsets.only(left: 70),
-              //             child: const Divider(
-              //                 height: 0,
-              //                 thickness: 1
-              //             ))
-              //       ],
-              //     )
-              //   ],
-              // )
+                        ListView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: listItem.length,
+                            itemBuilder: (context, indexChild) {
+                              Expenses? childItem = listItem.elementAt(indexChild);
+                              return listHeader.elementAt(index) == Utils.dateFormatYear(Utils.dateTimeFormat("${childItem.createDate}")) ?
+                              Column(
+                                children: [
+                                  ListItem(item: childItem),
+                                  childItem == listItem.last ? const SizedBox()
+                                      : Container(
+                                      padding: const EdgeInsets.only(left: 70),
+                                      child: const Divider(
+                                          height: 0,
+                                          thickness: 1
+                                      ))
+                                ],
+                              ) : const SizedBox();
+                            }
+                        )
+                      ]
+                  )
+              )
           );
         }
     );
